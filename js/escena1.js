a48f07c958e671d7b18fa35e55da106dd44e449d
gleidygraciano <gracianogleidyrubi@gmail.com>
2024-11-26 20:48:59 -0500

    Cambios-25nov

---
+++ js/escena1.js
@@ -0,0 +1,16 @@
+const imagecontainer = document.querySelector('.image-container')
+const narrativecontainer =  document.querySelector('.narrative-container')
+const narrative1 = document.querySelector('.narrative-1')
+const narrative2 = document.querySelector('.narrative-2')
+const image1 = document.querySelector('.image-1')
+const image2 =document.querySelector('.image-2') 
+
+setTimeout(() => {
+    image1.style.opacity = 0
+    image2.style.opacity = 1
+}, 8000)
+
+
+
+
+
